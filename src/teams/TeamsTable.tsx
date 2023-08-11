f7c1a1ba80a0d110c35aca7a8ce01c600da7cf44
Adrian Iuliu Moldovan <adrianiuliu.moldovan@yahoo.com>
2023-08-11 18:32:09 +0300

    moved teams data in TeamsTableWrapper

--- src/teams/TeamsTable.tsx
+++ src/teams/TeamsTable.tsx
@@ -31,7 +31,63 @@ function TeamRow(team) {
 }
 
 export function TeamsTable(props) {
-  console.info("table", props.loading);
+  console.info("table props", props);
+
+  return (
+    <form id="teamsForm" action="" method="get" className={props.loading ? "loading-mask" : ""}>
+      <table id="teamsTable">
+        <colgroup>
+          <col className="select-all-col" />
+          <col style={{ width: "20%" }} />
+          <col style={{ width: "40%" }} />
+          <col style={{ width: "25%" }} />
+          <col style={{ width: "15%" }} />
+          <col className="table-actions" />
+        </colgroup>
+        <thead>
+          <tr>
+            <th>
+              <input type="checkbox" name="selectAll" id="selectAll" />
+            </th>
+            <th>Promotion</th>
+            <th>Members</th>
+            <th>Project Name</th>
+            <th>Project URL</th>
+            <th>...</th>
+          </tr>
+        </thead>
+        <tbody>{props.teams.map(team => TeamRow(team))}</tbody>
+        <tfoot>
+          <tr>
+            <td></td>
+            <td>
+              <input type="text" name="promotion" placeholder="Enter Promotion" required />
+            </td>
+            <td>
+              <input type="text" name="members" placeholder="Enter Members" required />
+            </td>
+            <td>
+              <input type="text" name="name" placeholder="Enter Project Name" required />
+            </td>
+            <td>
+              <input type="text" name="url" placeholder="Enter Project URL" required />
+            </td>
+            <td>
+              <button className="action-btn" title="Add" type="submit">
+                ➕
+              </button>
+              <button className="action-btn" title="Cancel" type="reset">
+                ❌
+              </button>
+            </td>
+          </tr>
+        </tfoot>
+      </table>
+    </form>
+  );
+}
+
+export function TeamsTableWrapper() {
   const teams = [
     {
       id: "toze8j1610313009673",
@@ -74,58 +130,15 @@ export function TeamsTable(props) {
       createdBy: "nmatei"
     }
   ];
-  console.info("render table", teams);
-
   return (
-    <form id="teamsForm" action="" method="get" className={props.loading ? "loading-mask" : ""}>
-      <table id="teamsTable">
-        <colgroup>
-          <col className="select-all-col" />
-          <col style={{ width: "20%" }} />
-          <col style={{ width: "40%" }} />
-          <col style={{ width: "25%" }} />
-          <col style={{ width: "15%" }} />
-          <col className="table-actions" />
-        </colgroup>
-        <thead>
-          <tr>
-            <th>
-              <input type="checkbox" name="selectAll" id="selectAll" />
-            </th>
-            <th>Promotion</th>
-            <th>Members</th>
-            <th>Project Name</th>
-            <th>Project URL</th>
-            <th>...</th>
-          </tr>
-        </thead>
-        <tbody>{teams.map(team => TeamRow(team))}</tbody>
-        <tfoot>
-          <tr>
-            <td></td>
-            <td>
-              <input type="text" name="promotion" placeholder="Enter Promotion" required />
-            </td>
-            <td>
-              <input type="text" name="members" placeholder="Enter Members" required />
-            </td>
-            <td>
-              <input type="text" name="name" placeholder="Enter Project Name" required />
-            </td>
-            <td>
-              <input type="text" name="url" placeholder="Enter Project URL" required />
-            </td>
-            <td>
-              <button className="action-btn" title="Add" type="submit">
-                ➕
-              </button>
-              <button className="action-btn" title="Cancel" type="reset">
-                ❌
-              </button>
-            </td>
-          </tr>
-        </tfoot>
-      </table>
-    </form>
+    <>
+      <TeamsTable loading={true} teams={[]} />
+      <br />
+      <TeamsTable loading={false} teams={[]} />
+      <br />
+      <TeamsTable loading={true} teams={teams} />
+      <br />
+      <TeamsTable loading={false} teams={teams} />
+    </>
   );
 }
